0099286735bb8eabf6805173a11f5539c476a65e
huy <huynt57@gmail.com>
2014-02-27 23:35:56 +0700

    add side left navigation

--- protected/views/discussion/discussion.php
+++ protected/views/discussion/discussion.php
@@ -37,8 +37,94 @@
             <div class="g-cols">
                 <div class="one-fifth">
                     <h3>Các chủ đề</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque eveniet
-                        facere fugit nihil, non officiis temporibus! A adipisci alias veritatis?</p>
+                    <div class="widget">
+
+
+                        <nav class="w-nav">
+                            <div class="w-nav-h">
+                                <div class="w-nav-list layout_ver level_1">
+                                    <div class="w-nav-list-h">
+                                        <div class="w-nav-item level_1">
+                                            <div class="w-nav-item-h">
+                                                <a href="#" class="w-nav-anchor level_1">
+                                                    <span class="w-nav-title">Responsive Design</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="w-nav-item level_1 active">
+                                            <div class="w-nav-item-h">
+                                                <a href="#" class="w-nav-anchor level_1">
+                                                    <span class="w-nav-title">Retina Ready</span>
+                                                </a>
+                                                <div class="w-nav-list layout_ver place_inside level_2">
+                                                    <div class="w-nav-list-h">
+                                                        <div class="w-nav-item level_2">
+                                                            <div class="w-nav-item-h">
+                                                                <a href="#" class="w-nav-anchor level_2">
+                                                                    <span class="w-nav-title">Second Level Item</span>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="w-nav-item level_2 active">
+                                                            <div class="w-nav-item-h">
+                                                                <a href="#" class="w-nav-anchor level_2">
+                                                                    <span class="w-nav-title">Second Level Item</span>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="w-nav-item level_2">
+                                                            <div class="w-nav-item-h">
+                                                                <a href="#" class="w-nav-anchor level_2">
+                                                                    <span class="w-nav-title">Second Level Item</span>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="w-nav-item level_1">
+                                            <div class="w-nav-item-h">
+                                                <a href="#" class="w-nav-anchor level_1">
+                                                    <span class="w-nav-title">Animated Elements</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="w-nav-item level_1">
+                                            <div class="w-nav-item-h">
+                                                <a href="#" class="w-nav-anchor level_1">
+                                                    <span class="w-nav-title">Boxed & Wide Versions</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="w-nav-item level_1">
+                                            <div class="w-nav-item-h">
+                                                <a href="#" class="w-nav-anchor level_1">
+                                                    <span class="w-nav-title">Magnific Popup</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="w-nav-item level_1">
+                                            <div class="w-nav-item-h">
+                                                <a href="#" class="w-nav-anchor level_1">
+                                                    <span class="w-nav-title">Font Awesome Icons</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="w-nav-item level_1">
+                                            <div class="w-nav-item-h">
+                                                <a href="#" class="w-nav-anchor level_1">
+                                                    <span class="w-nav-title">Slider Revolution</span>
+                                                </a>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </nav>
+
+                    </div>
                 </div>
                 <div class="three-fifths">
                     <h3>Hãy cho ý kiến của bạn ...</h3>
