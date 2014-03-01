c5934540a840815f920845d9a1001eb694698b2a
Sonvn <vungocson94@gmail.com>
2014-03-01 17:28:52 +0700

    recommit

---
+++ protected/views/login/Login.php
@@ -0,0 +1,132 @@
+<!-- MAIN -->
+		<div class="l-submain">
+			<div class="l-submain-h g-html i-cf">
+				<div class="l-content">
+					<div class="l-content-h i-widgets">
+						<div class="g-cols">
+							
+							<div class="one-third" style="float:none; margin: 0 auto 0 auto;">
+								<div class="w-tabs layout_accordion with_icon">
+									<div class="w-tabs-h">
+										<div class="w-tabs-section with_icon active">
+											<div class="w-tabs-section-title">
+												<span class="w-tabs-section-title-icon fa fa-trophy">
+												</span>
+												<span class="w-tabs-section-title-text">Đăng Nhập</span>
+												<span class="w-tabs-section-title-control">
+													<i class="fa fa-angle-down"></i>
+												</span>
+											</div>
+											<div class="w-tabs-section-content" style="display: none;">
+												<div class="w-tabs-section-content-h">
+													<div class="wpb_text_column ">
+														<div class="wpb_wrapper">
+															<form class="g-form" action="" method="post">
+																<div class="g-form-group">
+																	<div class="g-form-group-rows">
+																		<div class="g-form-row">
+																			<div class="g-form-row-label">
+																				<label class="g-form-row-label-h" for="contact_email">Email của bạn *</label>
+																			</div>
+																			<div class="g-form-row-field">
+																				<div class="g-input">
+																					<input type="text" name="contact_email" id="contact_email" value="">
+																				</div>
+																			</div>
+																		</div>
+																		<div class="g-form-row">
+																			<div class="g-form-row-label">
+																				<label class="g-form-row-label-h" for="Password">Mật Khẩu *</label>
+																			</div>
+																			<div class="g-form-row-field">
+																				<div class="g-input">
+																					<input type="password" name="Password" id="Password" value="">
+																				</div>
+																			</div>
+																		</div>
+																		<div class="g-form-row">
+																			<div class="g-form-row-field">
+																				<button class="g-btn type_primary">Đăng Nhập</button>
+																			</div>
+																		</div>
+																		<div class="g-form-row">
+																			<div class="g-form-row-field">
+																				<button class="g-btn type_primary size_small">Đăng Nhập qua Facebook</button>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</form>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="w-tabs-section with_icon">
+											<div class="w-tabs-section-title">
+												<span class="w-tabs-section-title-icon fa fa-flask"></span>
+												<span class="w-tabs-section-title-text">Đăng Ký</span>
+												<span class="w-tabs-section-title-control">
+													<i class="fa fa-angle-down"></i>
+												</span>
+											</div>
+											<div class="w-tabs-section-content" style="display: none;">
+												<div class="w-tabs-section-content-h">
+													<div class="wpb_text_column ">
+														<div class="wpb_wrapper">
+															<form class="g-form" action="" method="post">
+																<input type="hidden" name="action" value="contact">
+																<div class="g-form-group">
+																	<div class="g-form-group-rows">
+																		<div class="g-form-row">
+																			<div class="g-form-row-label">
+																				<label class="g-form-row-label-h" for="new_user_name">Tên của bạn *</label>
+																			</div>
+																			<div class="g-form-row-field">
+																				<div class="g-input">
+																					<input type="text" name="contact_name" id="contact_name" value="">
+																				</div>
+																			</div>
+																		</div>
+																		<div class="g-form-row">
+																			<div class="g-form-row-label">
+																				<label class="g-form-row-label-h" for="contact_email">Email của bạn *</label>
+																			</div>
+																			<div class="g-form-row-field">
+																				<div class="g-input">
+																					<input type="text" name="contact_email" id="contact_email" value="">
+																				</div>
+																			</div>
+																		</div>
+																		<div class="g-form-row">
+																			<div class="g-form-row-label">
+																				<label class="g-form-row-label-h" for="Password">Mật Khẩu *</label>
+																			</div>
+																			<div class="g-form-row-field">
+																				<div class="g-input">
+																					<input type="password" name="Password" id="Password" value="">
+																				</div>
+																			</div>
+																		</div>
+																		<div class="g-form-row">
+																			<div class="g-form-row-field">
+																				<button class="g-btn type_primary">Đăng Ký</button>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</form>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<!-- /MAIN -->
