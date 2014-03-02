60feb1de021debb86e3cd1c85ab18ca34d0a48b0
Sonvn <vungocson94@gmail.com>
2014-03-02 11:24:06 +0700

    fix text-area in postdisscussion

--- protected/views/discussion/partial/postarea.php
+++ protected/views/discussion/partial/postarea.php
@@ -8,27 +8,25 @@
 					<span class="w-tabs-section-title-control"><i class="icon-angle-down"></i></span>
 				</div>
 				<div class="w-tabs-section-content">
-					<div class="w-tabs-section-content-h">
-						<form class="g-form" action="" method="post">
-		                    <input type="hidden" name="action" value="contact">
-		                    <div class="g-form-group">
-		                        <div class="g-form-group-rows">
-		                            <div class="g-form-row">
-		                                <div class="g-form-row-field">
-		                                    <div class="g-input">
-		                                        <textarea name="contact_message" id="contact_message" cols="30" rows="10"></textarea>
-		                                    </div>
-		                                </div>
-		                            </div>
-		                            <div class="g-form-row">
-		                                <div class="g-form-row-field">
-		                                    <button class="g-btn type_primary size_small">Đăng tin</button>
-		                                </div>
-		                            </div>
-		                        </div>
-		                    </div>
-                		</form>
-					</div>
+					<form class="g-form" action="" method="post">
+	                    <input type="hidden" name="action" value="contact">
+	                    <div class="g-form-group">
+	                        <div class="g-form-group-rows">
+	                            <div class="g-form-row" style="margin: 5px 0 5px 0; padding: 0;">
+	                                <div class="g-form-row-field">
+	                                    <div class="g-input">
+	                                        <textarea name="contact_message" id="contact_message" cols="30" rows="10"  style="border-color: #429EDB"></textarea>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                            <div class="g-form-row">
+	                                <div class="g-form-row-field">
+	                                    <button class="g-btn type_primary size_small">Đăng tin</button>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+               		</form>
 				</div>
 			</div>
 		</div>
