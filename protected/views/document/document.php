48f4f423540925fafb0dd8e93c0cbd0325ecf951
Son Vu <vungocson94@gmail.com>
2014-03-05 18:57:33 -0800

    doing document page

---
+++ protected/views/document/document.php
@@ -0,0 +1,194 @@
+<div class="l-submain">
+					<div class="l-submain-h g-html">
+
+						<div class="w-portfolio columns_4">
+							<div class="w-portfolio-h">
+								<div class="w-portfolio-list">
+									<div class="w-portfolio-list-h">
+									
+										<div class="w-portfolio-item order_1 naming webdesign">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-another-slider.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-1.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_2 naming branding">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-slider.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-2.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - Full Slider</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_3 webdesign illustration">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-slider-23.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-3.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - 2/3 Slider</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_4 photography naming">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-video.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-4.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - Full Video</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_5 webdesign">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-video-23.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-5.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - 2/3 Video</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_6 photography illustration">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-image.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-6.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - Full Image</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_7 naming branding">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-image-23.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-7.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - 2/3 Image</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_8 illustration branding">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-extended.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-8.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - Extended</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_9 photography illustration">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-another-slider.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-9.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - Another Slider</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+										
+										<div class="w-portfolio-item order_10 webdesign illustration">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-slider-23.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-10.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - 2/3 Slider</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_11 webdesign">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-video-23.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-11.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - 2/3 Video</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+										<div class="w-portfolio-item order_12 photography">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="project-image.html">
+													<div class="w-portfolio-item-image">
+														<img src="img/demo/portfolio-12.jpg" alt="" />
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title">Single Project - Full Image</h2>
+															<i class="icon-mail-forward"></i>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+
+									</div>
+								</div>
+								
+								<div class="w-portfolio-pagination">
+									<div class="g-pagination align_center">
+										<a href="javascript:void(0);" class="g-pagination-item to_prev disabled">Prev</a>
+										<a href="javascript:void(0);" class="g-pagination-item active">1</a>
+										<a href="javascript:void(0);" class="g-pagination-item">2</a>
+										<a href="javascript:void(0);" class="g-pagination-item">3</a>
+										<a href="javascript:void(0);" class="g-pagination-item to_next">Next</a>
+									</div>
+								</div>
+								
+							</div>
+						</div>
+
+					</div>
+				</div>
---
+++ protected/views/document/partial/item.php
@@ -0,0 +1,13 @@
+<div class="w-portfolio-item order_1 naming webdesign">
+	<div class="w-portfolio-item-h">
+		<a class="w-portfolio-item-anchor" href="project-another-slider.html">
+			<div class="w-portfolio-item-image">
+				<img src="img/demo/portfolio-1.jpg" alt="" />
+				<div class="w-portfolio-item-meta">
+					<h2 class="w-portfolio-item-title">Single Project</h2>
+					<i class="icon-mail-forward"></i>
+				</div>
+			</div>
+		</a>
+	</div>
+</div>
