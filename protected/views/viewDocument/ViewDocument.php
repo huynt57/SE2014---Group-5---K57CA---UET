22428f4429d2cb1e37d8e4a822d1479dd59145da
Son Vu <vungocson94@gmail.com>
2014-03-10 23:37:28 -0700

    finish view document page

--- protected/views/viewDocument/ViewDocument.php
+++ protected/views/viewDocument/ViewDocument.php
@@ -3,7 +3,7 @@
 		<div class="l-submain-h g-html i-cf">
             <div class="w-pagehead">
                 <h1 style="font-size: 30px;">Scribd in HTML5</h1>
-                <p>Published by</p>
+                <p>Đăng bởi</p>
                 <a href="#" style="font-size: 22px; margin: 10px 0 0 10px; float: left;">User</a>
             </div>
         </div>
@@ -11,13 +11,29 @@
     <div class="l-submain">
         <div class="l-submain-h g-html i-cf ">
             <div class="g-cols">
-            	<div class="three-fourths">
+            	<div class="four-fifths" style="float: none; margin: 0 auto">
             		<div class="l-content">
-            			<iframe class="scribd_iframe_embed" src="//www.scribd.com/embeds/30964170/content?start_page=1&view_mode=scroll&access_key=key-1ar9e5ms2364hpdfeixn&show_recommendations=true" data-auto-height="false" data-aspect-ratio="1.2938689217759" scrolling="no" id="doc_87674" width="100%" height="600" frameborder="0"></iframe>
+            			<iframe class="scribd_iframe_embed" src="//www.scribd.com/embeds/30964170/content?start_page=1&view_mode=scroll&access_key=key-1ar9e5ms2364hpdfeixn&show_recommendations=true" data-auto-height="true" data-aspect-ratio="1.2938689217759" scrolling="no" id="doc_87674" width="100%" height="600" frameborder="1"></iframe>
             		</div>
             	</div>
-            	<?php $this->renderPartial("partial/suggestion-document")?>
             </div>
+        </div>
+    </div>
+    <div class="l-submain-h g-html i-cf ">
+        <div class="g-cols">
+         	<div class="four-fifths" style="float: none; margin: 0 auto">
+		    	<button class="g-btn type_default size_small"><span><i class="icon-download-alt"></i>Download Tài liệu</span></button>
+				<button class="g-btn type_default size_small"><span><i class="icon-plus-sign-alt"></i>Thêm vào thư viện của bạn</span></button>
+			</div>
+		</div>
+    </div>
+    <div class="l-submain-h g-html i-cf ">
+        <div class="g-cols">
+         	<div class="four-fifths" style="float: none; margin: 0 auto">
+           		<div class="l-content">
+           			<?php $this->renderPartial("partial/comments")?>
+           		</div>
+           	</div>
         </div>
     </div>
 </div>
