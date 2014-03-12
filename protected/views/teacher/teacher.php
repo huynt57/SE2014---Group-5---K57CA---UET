b104a66c002983601d2be6bb3ce43f7b4de85107
kiennd57 <kiennd_570@.vnu.edu.vn>
2014-03-13 00:38:18 +0700

    Teacher Page!!!

--- protected/views/teacher/teacher.php
+++ protected/views/teacher/teacher.php
@@ -46,41 +46,148 @@
             </div>
         </div>
     </div>
-
+    
     <div class="l-submain">
-        <div class="g-cols">
-                <div class="full-width">
-                        <div class="l-submain-h g-html i-cf">
-                            <div class="g-cols">
-                                <div class="one-third">
-                                    <span style="text-align: center; font-size: 300%">Tiểu sử</span>
-                                </div>
-                                <div class="two-thirds">
-                                    <div class="box1" style="height: 300px;">
+            <div class="l-submain-h g-html">
 
+                    <h2>Sơ lược</h2>
+
+                    <div class="g-hr type_short">
+                            <span class="g-hr-h">
+                                    <i class="icon-arrow-down"></i>
+                            </span>
+                    </div>
+
+                    <div class="g-cols">
+                            <div class="one-half">
+                                    <div class="w-iconbox icon_left">
+                                            <div class="w-iconbox-h">
+                                                    <div class="w-iconbox-icon">
+                                                            <i class="icon-html5"></i>
+                                                    </div>
+                                                    <div class="w-iconbox-text">
+                                                            <h3 class="w-iconbox-text-title">Tiểu sử</h3>
+                                                            <div class="w-iconbox-text-description">
+                                                                    <p>Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.</p>
+                                                            </div>
+                                                            <a class="w-iconbox-text-link" href="#"><span>Learn more</span></a>
+                                                    </div>
+                                            </div>
                                     </div>
+                            </div>
+                            <div class="one-half">
+                                    <div class="w-iconbox icon_left">
+                                            <div class="w-iconbox-h">
+                                                    <div class="w-iconbox-icon">
+                                                            <i class="icon-trophy"></i>
+                                                    </div>
+                                                    <div class="w-iconbox-text">
+                                                            <h3 class="w-iconbox-text-title">Công trình nghiên cứu</h3>
+                                                            <div class="w-iconbox-text-description">
+                                                                    <p>Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.</p>
+                                                            </div>
+                                                            <a class="w-iconbox-text-link" href="#"><span>Learn more</span></a>
+                                                    </div>
+                                            </div>
+                                    </div>
+                            </div>
+                    </div>
+
+                    <div class="g-hr type_invisible">
+                            <span class="g-hr-h">
+                                    <i class="icon-star"></i>
+                            </span>
+                    </div>
+
+                    <div class="g-cols">
+                            <div class="one-half">
+                                    <div class="w-iconbox icon_left">
+                                            <div class="w-iconbox-h">
+                                                    <div class="w-iconbox-icon">
+                                                            <i class="icon-magic"></i>
+                                                    </div>
+                                                    <div class="w-iconbox-text">
+                                                            <h3 class="w-iconbox-text-title">Tính cách</h3>
+                                                            <div class="w-iconbox-text-description">
+                                                                    <p>Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.</p>
+                                                            </div>
+                                                            <a class="w-iconbox-text-link" href="#"><span>Learn more</span></a>
+                                                    </div>
+                                            </div>
+                                    </div>
+                                    <br/>
+                                    <br/>
+                                    <div class="w-iconbox icon_left">
+                                            <div class="w-iconbox-h">
+                                                    <div class="w-iconbox-icon">
+                                                            <i class="icon-code"></i>
+                                                    </div>
+                                                    <div class="w-iconbox-text">
+                                                            <h3 class="w-iconbox-text-title">Lời khuyên</h3>
+                                                            <div class="w-iconbox-text-description">
+                                                                    <p>Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.</p>
+                                                            </div>
+                                                            <a class="w-iconbox-text-link" href="#"><span>Learn more</span></a>
+                                                    </div>
+                                            </div>
+                                    </div>
+                            </div>
+                            <div class="one-half">
+                                <div class="w-tabs layout_accordion with_icon">
+                                        <div class="w-tabs-h">
+                                                <div class="w-tabs-section with_icon active">
+                                                        <div class="w-tabs-section-title">
+                                                                <span class="w-tabs-section-title-icon icon-desktop"></span>
+                                                                <span class="w-tabs-section-title-text">Comments</span>
+                                                                <span class="w-tabs-section-title-control"><i class="icon-angle-down"></i></span>
+                                                        </div>
+                                                        <div class="w-tabs-section-content">
+                                                                <div class="w-tabs-section-content-h">
+                                                                        <p><strong>Đưa ra những điều bạn nghĩ</strong></p>
+                                                                        <form class="g-form" action="#" method="post" />
+                                                                        <div class="g-form-group">
+                                                                            <div class="g-form-group-rows">
+
+                                                                                <div class="g-form-row" style="margin-right: 5%; padding-bottom: 4%">
+
+                                                                                    <div class="g-form-row-field">
+                                                                                        <textarea name="#" id="input1x3" cols="30" rows="10"></textarea>
+                                                                                    </div>
+                                                                                </div>
+                                                                                <div class="g-form-row">
+                                                                                    <div class="g-form-row-label"></div>
+                                                                                    <div class="g-form-row-field">
+                                                                                        <button class="g-btn type_primary size_small">Send</button>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                        </form>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                                <div class="w-tabs-section with_icon">
+                                                        <div class="w-tabs-section-title">
+                                                                <span class="w-tabs-section-title-icon icon-male"></span>
+                                                                <span class="w-tabs-section-title-text">Rate</span>
+                                                                <span class="w-tabs-section-title-control"><i class="icon-angle-down"></i></span>
+                                                        </div>
+                                                        <div class="w-tabs-section-content">
+                                                                <div class="w-tabs-section-content-h">
+                                                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </div>
                                 </div>
                             </div>
-                        </div>
-                </div>
-        </div>
-    </div>
-    <div class="l-submain">
-        <div class="g-cols">
-            <div class="full-width">
-                <div class="l-submain-h g-html i-cf">
-                        <div class="g-cols">
-                            <div class="one-third">
-                                <span style="text-align: center; font-size: 300%">Tính cách</span>
-                            </div>
-                            <div class="two-thirds">
-                                <div class="box1" style="height: 300px;">
-                                </div>
-                            </div>
-                        </div>
+                    </div>
+
+                    <div class="g-hr type_invisible">
+                            <span class="g-hr-h">
+                                    <i class="icon-star"></i>
+                            </span>
                     </div>
             </div>
-        </div>
     </div>
-         
 </div>
