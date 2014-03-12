5902330f09869fad144066f60bdc4d0f4a96a474
Son Vu <vungocson94@gmail.com>
2014-03-11 19:14:04 -0700

    add an icon to button link to facebook

--- protected/views/login/Login.php
+++ protected/views/login/Login.php
@@ -109,7 +109,8 @@
                                                                 <div class="g-form-row">
                                                                     <div class="g-form-row-field">
                                                                         <button class="g-btn type_primary size_small" style="background-color: #1265A8">
-                                                                            Đăng Nhập qua Facebook
+                                                                            <i class="icon-facebook"></i>
+                                                                            <span>Đăng Nhập qua Facebook</span>
                                                                         </button>
                                                                     </div>
                                                                 </div>
