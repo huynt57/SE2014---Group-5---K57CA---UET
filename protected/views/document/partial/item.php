d972bf7fd6cc906ff1dc47404427bec552824fb9
huy <huynt57@gmail.com>
2014-03-14 01:00:16 +0700

    fix upload document

--- protected/views/document/partial/item.php
+++ protected/views/document/partial/item.php
@@ -2,7 +2,7 @@
 	<div class="w-portfolio-item-h">
 		<a class="w-portfolio-item-anchor" href="project-another-slider.html">
 			<div class="w-portfolio-item-image fixsizeitems fixmarginbottom">
-				<img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/portfolio-1.jpg" class="fixsizeitems" alt="" />
+                            <img src="http://imgv2-4.scribdassets.com/img/word_document/212235756/400x1000/2ae0720b98/1394707109" class="fixsizeitems" alt="" style="border-style: solid; border-width: thin; border-color: #d0d6d9;"/>
 				<div class="w-portfolio-item-meta">
 					<h2 class="w-portfolio-item-title">Single Project</h2>
 					<i class="icon-mail-forward"></i>
