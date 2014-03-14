ae74de5a29422affe574800795101398703b2427
kiennd57 <kiennd_570@.vnu.edu.vn>
2014-03-14 08:06:53 +0700

    Teacher Page-final!

--- protected/views/teacher/teacher.php
+++ protected/views/teacher/teacher.php
@@ -8,47 +8,44 @@
     </div>
 
     <div class="l-submain">
-        <div class="g-cols">
-                <div class="full-width">
-                    <div class="l-submain-h g-html i-cf">
-                        <div class="l-submain-h g-html i-cf">
-                            <div class="g-cols">
-                                <div class="one-third">
-                                    <img class="rounded1 float_left" src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/serv2.jpg"/>
-                                </div>
-                                <div class="two-thirds">
-                                    <div class="box1" style="height: 150px;">
-                                        <div class="g-cols">
-                                            <div class="three-fifths">
-                                                <span class="inset-text">Nguyễn Văn Huy</span>
-                                                <br/>
-                                                <br/>
-                                                <span>PGS.TS Ngành Công Nghệ-Thông Tin</span>
-                                                <br/>
-                                                <span>Cần tuyển người yêu ngay lập tức</span>
-                                                <br/>
-                                                <span>Yêu cầu: to béo, lùn</span>
-                                            </div>
-                                            <div class="two-fifths">
-                                                <span>số 1, Đức Diễn, Từ Liêm, Hà Nội</span>
-                                                <br/>
-                                                <span>Email: huyngu@gmail.com</span>
-                                                <br/>
-                                                <span>SĐT: 010101010</span>
-                                                <br/>
+            <div class="l-submain-h g-html">
+                    <div class="g-cols">
+                        <div class="full-width">
+                            <div class="l-submain-h g-html i-cf">
+                                <div class="l-submain-h g-html i-cf">
+                                    <div class="g-cols">
+                                        <div class="one-third">
+                                            <img class="rounded1 float_left" src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/serv2.jpg"/>
+                                        </div>
+                                        <div class="two-thirds">
+                                            <div class="box1" style="height: 150px;">
+                                                <div class="g-cols">
+                                                    <div class="three-fifths">
+                                                        <span class="inset-text">Nguyễn Văn Huy</span>
+                                                        <br/>
+                                                        <br/>
+                                                        <span>PGS.TS Ngành Công Nghệ-Thông Tin</span>
+                                                        <br/>
+                                                        <span>Cần tuyển người yêu ngay lập tức</span>
+                                                        <br/>
+                                                        <span>Yêu cầu: to béo, lùn</span>
+                                                    </div>
+                                                    <div class="two-fifths">
+                                                        <span>số 1, Đức Diễn, Từ Liêm, Hà Nội</span>
+                                                        <br/>
+                                                        <span>Email: huyngu@gmail.com</span>
+                                                        <br/>
+                                                        <span>SĐT: 010101010</span>
+                                                        <br/>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
                                 </div>
-                        </div>
+                            </div>
+                           </div>
                     </div>
-                   </div>
-            </div>
-        </div>
-    </div>
-    
-    <div class="l-submain">
-            <div class="l-submain-h g-html">
+                </div>
 
                     <h2>Sơ lược</h2>
 
