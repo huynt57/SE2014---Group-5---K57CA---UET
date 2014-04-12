77593fd3845c73e9c77d03764fab0bd5409f4902
Nguyen Duy Kien <kienduynguyen94@gmail.com>
2014-04-13 01:19:41 +0700

    reconstruct class bar and information bar user page!

--- protected/views/user/user.php
+++ protected/views/user/user.php
@@ -44,11 +44,13 @@
                                 <b>Details</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Hanoi, Vietnam.
                                 <br/>
                                 <b>Activity</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Joined in March 2013, Vip member of website - boss. 
+                                <br/>
+                                <b>Quote</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Change words, Change world.
                             </div>
 
                             <div style="text-align: center">
                                 <p>
-                                    <a href="">Send he a mess</a> | <a href="">Add he to a group</a> | <a href="">Send friend request</a>
+                                    <a href="">Add he/she to a class</a> | <a href="">Add he/she to a group</a> | <a href="">Suggest he/she books</a>
                                 </p>
                             </div>
 
@@ -105,14 +107,6 @@
                             </div>
 
                             <a href="" style="margin-left: 30px">More...</a>
-
-                            <div class="rounded1 color_alternate">
-                                <h6>Kien's quotes</h6>
-                            </div>
-                            
-                            <div>
-                                Kien hasn't added any quotes yet.<a href="">Add quotes now >></a>
-                            </div>
 
                             <div class="rounded1 color_alternate">
                                 <h6>Kien's recent updates</h6>
@@ -237,11 +231,34 @@
             </div>
 
             <div class="one-third">
-                <div class="rounded2 color_alternate" style="margin-top: 70px">
+                
+                <div class="rounded2 color_alternate"  style="margin-top: 70px">
+                    <h6>Kien's Information</h6>
+                </div>
+                
+                <div>
+                    <i class="icon-user"></i>
+                    <span>&nbsp Working at &nbsp : &nbsp &nbsp &nbsp &nbsp &nbsp <span style="color: #B0B3B5">UET-bluebee team</span> </span>
+                    <br/>
+                    <i class="icon-phone"></i>
+                    <span>&nbsp Phone &nbsp : &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <span style="color: #B0B3B5">01666107083</span> </span>
+                    <br/>
+                    <i class="icon-home"></i>
+                    <span>&nbsp Live at &nbsp : &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <span style="color: #B0B3B5">Ha Noi, Viet Nam</span> </span>
+                    <br/>
+                    <i class="icon-heart"></i>
+                    <span>&nbsp Relationship &nbsp : &nbsp &nbsp &nbsp <span style="color: #BABDBF">Forever alone</span> </span>
+                    <br/>
+                    <i class="icon-group"></i>
+                    <span>&nbsp Join in &nbsp : &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <span style="color: #B0B3B5">13/1/2014</span> </span>
+                    <br/>
+                </div>
+                
+                <div class="rounded2 color_alternate" style="margin-top: 10px">
                     <h6>Class Kien follows</h6>
                 </div>
                 
-                <div class="w-portfolio columns_4 wide-margins type_sortable">
+                <div class="w-portfolio columns_2 wide-margins type_sortable">
                     <div class="w-portfolio-h">
                         <div class="w-portfolio-list">
                             <div class="w-portfolio-list-h">
@@ -363,27 +380,6 @@
                     <a href="">
                     <button class="g-btn type_outline"><span><i class="icon-heart"></i>Creat your own group</span></button>
                     </a>
-                </div>
-
-                <div class="rounded2 color_alternate">
-                    <h6>Kien's Information</h6>
-                </div>
-                
-                <div>
-                    <i class="icon-user"></i>
-                    <span>&nbsp Working at &nbsp : &nbsp &nbsp &nbsp &nbsp &nbsp <span style="color: #B0B3B5">UET-bluebee team</span> </span>
-                    <br/>
-                    <i class="icon-phone"></i>
-                    <span>&nbsp Phone &nbsp : &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <span style="color: #B0B3B5">01666107083</span> </span>
-                    <br/>
-                    <i class="icon-home"></i>
-                    <span>&nbsp Live at &nbsp : &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <span style="color: #B0B3B5">Ha Noi, Viet Nam</span> </span>
-                    <br/>
-                    <i class="icon-heart"></i>
-                    <span>&nbsp Relationship &nbsp : &nbsp &nbsp &nbsp <span style="color: #BABDBF">Forever alone</span> </span>
-                    <br/>
-                    <i class="icon-group"></i>
-                    <span>&nbsp Join in &nbsp : &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <span style="color: #B0B3B5">13/1/2014</span> </span>
                 </div>
 
             </div>
