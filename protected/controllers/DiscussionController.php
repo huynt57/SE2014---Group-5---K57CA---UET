7d61ab212553d960141d9750f8ec212430981a32
Khanh NGUYEN <khanhnv3007@gmail.com>
2014-04-12 20:55:08 +0700

    Fixed discussion coding standard

--- protected/controllers/DiscussionController.php
+++ protected/controllers/DiscussionController.php
@@ -1,5 +1,7 @@
 <?php
+
 Yii::import('application.controllers.BaseController');
+
 class DiscussionController extends BaseController {
 
     public function actionIndex() {
@@ -11,29 +13,25 @@ class DiscussionController extends BaseController {
         $newCriteria->select = "*";
         $newCriteria->order = "post_id DESC";
         $this->render('discussion', array('post' => Post::model()->findAll($newCriteria)));
-        
     }
 
-    public function actionMakePost(){
-        
+    public function actionMakePost() {
+
         $this->retVal = new stdClass();
         $request = Yii::app()->request;
         if ($request->isPostRequest && isset($_POST)) {
             try {
                 $loginFormData = array(
                     'post_content' => $_POST['post_content'],
-                    
                 );
                 $post_model = new Post;
                 $post_model->post_content = $loginFormData['post_content'];
 
                 $post_model->save(FALSE);
-                if ($post_model->save(FALSE))
-                {
+                if ($post_model->save(FALSE)) {
                     $this->retVal->success = TRUE;
-                }
-                else{
-                     $this->retVal->success = FALSE;
+                } else {
+                    $this->retVal->success = FALSE;
                 }
             } catch (exception $e) {
                 // $this->retVal->message = $e->getMessage();
@@ -43,6 +41,7 @@ class DiscussionController extends BaseController {
         echo CJSON::encode($this->retVal);
         Yii::app()->end();
     }
+
     // Uncomment the following methods and override them if needed
     /*
       public function filters()
