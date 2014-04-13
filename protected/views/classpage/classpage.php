1b4bd9dd9837cdb42f4d7be24f0ddf235b158a0f
Son Vu <vungocson94@gmail.com>
2014-04-13 20:46:25 +0700

    done layout of classpage

---
+++ protected/views/classpage/classpage.php
@@ -0,0 +1,178 @@
+<div class="l-submain-h i-cf">
+    <div class="l-content">
+        <div class="l-content-h i-widgets">
+            <style>
+                #cover {
+                    background-image: url(<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/cover.jpg);
+                    min-height: 400px;
+                }
+                #button-change-cover {
+                    top: 350px;
+                    left: 850px;
+                }
+                #fix-style-w-tab {
+                    border-top: none;
+                    border-left: 1px solid #d0d6d9;
+                    border-bottom: none;
+                    border-right: 1px solid #d0d6d9;
+                    min-height: 500px;
+                }
+                .fix-w-tab-item {
+                    width: 25%;
+                    text-align: center;
+                }
+            </style>
+            <div id="cover">
+                <button class="g-btn type_primary size_small" id="button-change-cover">
+                    <span>Change Cover</span>
+                </button>
+            </div>
+            <div class="g-cols">
+                <div class="two-thirds">
+                        <!-- w-tabs -->
+                        <div class="w-tabs">
+                            <div class="w-tabs-h" id="fix-style-w-tab">
+                                <div class="w-tabs-list">
+                                    <style>
+                                        
+                                    </style>
+                                    <div class="w-tabs-item fix-w-tab-item active">
+                                        <span class="w-tabs-item-icon"></span>
+                                        <span class="w-tabs-item-title">Activity</span>
+                                    </div>
+
+                                    <div class="w-tabs-item fix-w-tab-item">
+                                        <span class="w-tabs-item-icon"></span>
+                                        <span class="w-tabs-item-title">Documents</span>
+                                    </div>
+
+                                    <div class="w-tabs-item fix-w-tab-item">
+                                        <span class="w-tabs-item-icon"></span>
+                                        <span class="w-tabs-item-title">Information</span>
+                                    </div>
+
+                                    <div class="w-tabs-item fix-w-tab-item">
+                                        <span class="w-tabs-item-icon"></span>
+                                        <span class="w-tabs-item-title">Members</span>
+                                    </div>
+                                </div>
+
+                                <div class="w-tabs-section active">
+                                    <div class="w-tabs-section-title">
+                                        <span class="w-tabs-section-title-icon"></span>
+                                        <span class="w-tabs-section-title-text">Activity</span>
+                                        <span class="w-tabs-section-title-control"></span>
+                                    </div>
+                                    <div class="w-tabs-section-content">
+                                        <div class="w-tabs-section-content-h">
+                                            <input>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="w-tabs-section">
+                                    <div class="w-tabs-section-title">
+                                        <span class="w-tabs-section-title-icon"></span>
+                                        <span class="w-tabs-section-title-text">Documents</span>
+                                        <span class="w-tabs-section-title-control"></span>
+                                    </div>
+                                    <div class="w-tabs-section-content">
+                                        <div class="w-tabs-section-content-h">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla suctus. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                            <div class="g-cols">
+                                                <div class="one-half">
+                                                    <h4>One Half</h4>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo.</p>
+                                                </div>
+                                                <div class="one-half">
+                                                    <h4>One Half</h4>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="w-tabs-section">
+                                    <div class="w-tabs-section-title">
+                                        <span class="w-tabs-section-title-icon"></span>
+                                        <span class="w-tabs-section-title-text">Information</span>
+                                        <span class="w-tabs-section-title-control"></span>
+                                    </div>
+                                    <div class="w-tabs-section-content">
+                                        <div class="w-tabs-section-content-h">
+                                            <div class="g-cols">
+                                                <div class="one-third">
+                                                    <img src="img/demo/team-1.jpg" alt="" />
+                                                </div>
+                                                <div class="one-third">
+                                                    <h4>One Third</h4>
+                                                    <p>At delectus doloremque dolores explicabo laudantium minima qui. Animi aperiam
+                                                        aspernatur atque debitis distinctio impedit inventore iure labore modi omnis,
+                                                        optio rerum ut veritatis voluptatum?</p>
+                                                </div>
+                                                <div class="one-third">
+                                                    <h4>One Third</h4>
+                                                    <p>Accusamus et hic inventore iure iusto modi reprehenderit soluta. Aliquam,
+                                                        assumenda at consequuntur cumque, enim, explicabo iusto libero maiores nisi
+                                                        numquam odio porro praesentium quis.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="w-tabs-section">
+                                    <div class="w-tabs-section-title">
+                                        <span class="w-tabs-section-title-icon"></span>
+                                        <span class="w-tabs-section-title-text">Members</span>
+                                        <span class="w-tabs-section-title-control"></span>
+                                    </div>
+                                    <div class="w-tabs-section-content">
+                                        <div class="w-tabs-section-content-h">
+                                            <div class="g-cols">
+                                                <div class="one-third">
+                                                    <img src="img/demo/team-1.jpg" alt="" />
+                                                </div>
+                                                <div class="one-third">
+                                                    <h4>One Third</h4>
+                                                    <p>At delectus doloremque dolores explicabo laudantium minima qui. Animi aperiam
+                                                        aspernatur atque debitis distinctio impedit inventore iure labore modi omnis,
+                                                        optio rerum ut veritatis voluptatum?</p>
+                                                </div>
+                                                <div class="one-third">
+                                                    <h4>One Third</h4>
+                                                    <p>Accusamus et hic inventore iure iusto modi reprehenderit soluta. Aliquam,
+                                                        assumenda at consequuntur cumque, enim, explicabo iusto libero maiores nisi
+                                                        numquam odio porro praesentium quis.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                </div>
+                <div class="one-third">
+                    <h3 style="margin-top: 20px">Lớp: <br>Công Nghệ Phần Mềm</h3>
+                    <p><strong>Mã Môn Học:</strong> INT2208</p>
+                    <p><strong>Số tín chỉ:</strong> 3</p>
+                    <p><strong>Website Môn Học:</strong> <a href="bluebee-uet.com">bluebee-uet.com</a></p>
+                    <p><strong>Sơ Lược Môn Học:</strong> bla bal lab lba</p>
+                    <div class="g-hr">
+                        <span class="g-hr-h">
+                            <i class="icon-user"></i>
+                        </span>
+                    </div>
+                    <h3>Teacher(2)</h3>
+                    <ul>
+                        <li><strong>Client</strong>: ThemeForest</li>
+                        <li><strong>Date</strong>: April 16, 2013</li>
+                        <li><strong>Project URL</strong>:&nbsp;<a href="#">www.yoursite.com</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
